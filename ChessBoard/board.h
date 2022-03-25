cf2dcff019fb7caa979f7e385002076b8d7dce76
kaojonathan <kaojonathan1@gmail.com>
2022-03-25 06:35:35 -0400

    made dtor public for board

--- ChessBoard/board.h
+++ ChessBoard/board.h
@@ -5,7 +5,6 @@
 
 class Board {
 	protected:
-	virtual ~Board();
 	virtual void origSetup() = 0;
 	virtual void print() = 0;	
 	virtual bool end() = 0; // needs implementation
@@ -13,12 +12,14 @@ class Board {
 	virtual void insertNewPiece(std::string validpiece, std::string position) = 0;
 	virtual void removePiece(std::string position)= 0;
 	virtual bool verifySetup() = 0;
+	// will NVI UMP
 	virtual void updateMovePossibilities() = 0; // updates each Piece's possible moves (uses polymorphism)
 	virtual Piece* getPiece(int x, int y) = 0;
 	virtual void movePiece(int x, int y, int newx, int newy) = 0; // setter
 	virtual bool validPos (int x, int y) = 0;
 
 	public:
+	virtual ~Board();
 	bool checkPos(int, int);
 };
 
