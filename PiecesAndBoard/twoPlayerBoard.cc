602315579c117af465534dbb06b2ec5e125c9927
vincent-t-wan <wanv2002@gmail.com>
2022-03-30 21:48:53 -0400

    minor changes

--- PiecesAndBoard/twoPlayerBoard.cc
+++ PiecesAndBoard/twoPlayerBoard.cc
@@ -47,7 +47,7 @@ void twoPlayerBoard::movePiece(int x, int y, int newx, int newy)
 				 (board[newy][newx]->getRep() <= "Z")))
 			{
 
-				delete board[newy][newx]; // we can make it track the number of pieces left by making the destructor body decrement a field. (I didn't implement yet)
+		
 
 				board[newy][newx] = board[y][x];
 				board[y][x] = nullptr;
@@ -58,8 +58,6 @@ void twoPlayerBoard::movePiece(int x, int y, int newx, int newy)
 					 (("a" <= board[newy][newx]->getRep()) &&
 					  (board[newy][newx]->getRep() <= "z")))
 			{
-
-				delete board[newy][newx]; // we can make it track the number of pieces left by making the destructor body decrement a field. (I didn't implement yet)
 
 				board[newy][newx] = board[y][x];
 				board[y][x] = nullptr;
--- Players/player.cc
+++ Players/player.cc
@@ -7,18 +7,30 @@ using namespace std;
 
 Player::Player(int side, int type) : side{side}, type{type} {}
 
-Player::~Player() {}
-
-
-void Player::kingCheckedBy(Piece * enemy) {
-    opponentCheck = enemy;
-    for (auto piece : pieces) piece->forcedBy(enemy, true);
+Player::~Player()
+{
+    while (pieces.size() > 0)
+    {
+        delete pieces.back();
+        pieces.pop_back();
+    }
+    delete king;
 }
 
-void Player::removePiece(std::pair<int,int> pos) {// removes the piece in position (x, y) and places it in inactivePieces.
+void Player::kingCheckedBy(Piece *enemy)
+{
+    opponentCheck = enemy;
+    for (auto piece : pieces)
+        piece->forcedBy(enemy, true);
+}
 
-    for (int i = 0; i < pieces.size(); ++i) {
-        if ((pieces[i]->getX() == pos.first) && (pieces[i]->getY() == pos.second)) {
+void Player::removePiece(std::pair<int, int> pos)
+{ // removes the piece in position (x, y) and places it in inactivePieces.
+
+    for (int i = 0; i < pieces.size(); ++i)
+    {
+        if ((pieces[i]->getX() == pos.first) && (pieces[i]->getY() == pos.second))
+        {
             inactivePieces.emplace_back(pieces[i]); // emplace
             auto it = pieces.begin() + i;
             it = pieces.erase(it); // remove
@@ -27,36 +39,48 @@ void Player::removePiece(std::pair<int,int> pos) {// removes the piece in positi
 }
 
 // update status of all pieces of the player, return 0 if there is avaliable move for player, 1 if it is a checkmate, 2 if it is a draw.
-int Player::checkStatus(){
-    for (auto piece : pieces) piece->statusUpdate();
-    king->statusUpdate();           // not sure if we need to do this with two kings together
-    if (canMove()) return 0;        // the player can move a piece
-    if (opponentCheck) return 1;    // checkmate
+int Player::checkStatus()
+{
+    for (auto piece : pieces)
+        piece->statusUpdate();
+    king->statusUpdate(); // not sure if we need to do this with two kings together
+    if (canMove())
+        return 0; // the player can move a piece
+    if (opponentCheck)
+        return 1; // checkmate
     return 2;
 }
 
 // unset that status fields of each piece
-void Player::unsetStatus(){
-    for (auto piece : pieces) piece->needsUpdate();
+void Player::unsetStatus()
+{
+    for (auto piece : pieces)
+        piece->needsUpdate();
     king->needsUpdate();
 }
 
 // find all piece of Player can attack position pos
-vector<Piece *> Player::canAttack(pair<int, int> pos){
+vector<Piece *> Player::canAttack(pair<int, int> pos)
+{
     vector<Piece *> res{};
-    if (king->canAttack(pos)) res.emplace_back(king);
-    for(auto piece : pieces) {
-        if (piece->canAttack(pos)) res.emplace_back(piece);
+    if (king->canAttack(pos))
+        res.emplace_back(king);
+    for (auto piece : pieces)
+    {
+        if (piece->canAttack(pos))
+            res.emplace_back(piece);
     }
     return res;
 }
 
 // true if there is any pieces that the player can move
-bool Player::canMove() {
-    for (auto piece : pieces){
-        if ((piece->getMoves().size() != 0) || (piece->getTargets().size() != 0)) return true;
+bool Player::canMove()
+{
+    for (auto piece : pieces)
+    {
+        if ((piece->getMoves().size() != 0) || (piece->getTargets().size() != 0))
+            return true;
     }
-    if ((king->getMoves().size() != 0) || (king->getTargets().size() != 0)) return true;
+    if ((king->getMoves().size() != 0) || (king->getTargets().size() != 0))
+        return true;
 }
-
-
