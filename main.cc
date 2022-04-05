549e00ba5b46a5b369304da652d421f9fad1be1d
kaojonathan <kaojonathan1@gmail.com>
2022-04-05 09:51:12 -0400

    documentation update

--- main.cc
+++ main.cc
@@ -2,14 +2,13 @@
 #include <stdexcept>
 using namespace std;
 
-// implemented using help from https://www.youtube.com/watch?v=44tO977slsU
-// (type "game loop" and do some research)
+// "game loop" idea inspired by https://www.youtube.com/watch?v=44tO977slsU
 int main(int argc, const char *argv[])
 {
 	Game game;
 	game.init(); // initialize the game
 	while (game.running())
-	{ // the game loop
+	{
 		try
 		{
 			// handle input commands
@@ -18,10 +17,10 @@ int main(int argc, const char *argv[])
 			game.update();
 		}
 		catch (runtime_error &e)
-		{ // catch errors or restarts
+		{ // catch invalid input
 			cout << e.what() << endl;
 		}
 	}
-	game.quit(); // quit the game once loop ends
+	game.quit(); 
 	return 0;
 }
